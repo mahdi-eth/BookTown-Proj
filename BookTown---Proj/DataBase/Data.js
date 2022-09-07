ef629e3fbc8ccc537a2076a2fcb1783760a4c43a
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-09-07 06:36:38 +0430

    Add files via upload

--- BookTown---Proj/DataBase/Data.js
+++ BookTown---Proj/DataBase/Data.js
@@ -6,20 +6,509 @@ function books() {
       name: "Verity",
       price: "$11.26",
       date: 1,
-      rating:`<div class="ratings">
+      deal: false,
+
+      // 3.5
+      rating: `<div class="ratings">
             <i class="bi bi-star-fill text-warning"></i>
             <i class="bi bi-star-fill text-warning"></i>
             <i class="bi bi-star-fill text-warning"></i>
             <i class="bi bi-star-half text-warning"></i>
             <i class="bi bi-star text-warning"></i>
-        </div>`,
+            </div>`,
     },
     {
       cover:
         "https://images-na.ssl-images-amazon.com/images/I/71PNGYHykrL._AC_UL254_SR254,254_.jpg",
       name: "It Starts with Us: A Novel (2) (It Ends with Us)",
-      price: "$11.26",
+      price: "$12.99",
       date: 2,
+      deal: false,
+
+      // 4
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/71j0FLAauxL._AC_UL254_SR254,254_.jpg",
+      name: "It Ends with Us: A Novel (1)",
+      price: "$10.26",
+      date: 3,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61QR7qoEYVL._AC_UL254_SR254,254_.jpg",
+      name: "Ugly Love: A Novel",
+      price: "$10.40",
+      date: 4,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/81PMOtoT7zL._AC_UL254_SR254,254_.jpg",
+      name: "Fairy Tale",
+      price: "$20.87",
+      date: 5,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL._AC_UL254_SR254,254_.jpg",
+      name: "Reminders of Him: A Novel",
+      price: "$9.57",
+      date: 6,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/714l-Ja-VAL._AC_UL127_SR127,127_.jpg",
+      name: "I'm Glad My Mom Died",
+      price: "$18.70",
+      date: 7,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/81O1oy0y9eL._AC_UL254_SR254,254_.jpg",
+      name: "Where the Crawdads Sing",
+      price: "$9.98",
+      date: 8,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL254_SR254,254_.jpg",
+      name: "Atomic Habits",
+      price: "$11.98",
+      date: 9,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/71cTtxm0p0L._AC_UL127_SR127,127_.jpg",
+      name: "Solito: A Memoir",
+      price: "$21.12",
+      date: 10,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61xkvfPVupL._AC_UL127_SR127,127_.jpg",
+      name: "November 9: A Novel",
+      price: "$11.44",
+      date: 11,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61NdJMwAThS._AC_UL127_SR127,127_.jpg",
+      name: "The Body Keeps the Score",
+      price: "$11.40",
+      date: 12,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/91cqEsyjd-L._AC_UL127_SR127,127_.jpg",
+      name: "The Very Hungry Caterpillar",
+      price: "$4.30",
+      date: 13,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/81spBNattuL._AC_UL127_SR127,127_.jpg",
+      name: "Dinner in One",
+      price: "$24.99",
+      date: 14,
+      deal: false,
+
+      // 3.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            <i class="bi bi-star text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/61uiYWcEQGL._AC_UL127_SR127,127_.jpg",
+      name: "The Return of the Gods",
+      price: "$18.29",
+      date: 15,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL127_SR127,127_.jpg",
+      name: "The 48 Laws of Power",
+      price: "$11.60",
+      date: 16,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/81hHy5XrdKL._AC_UL127_SR127,127_.jpg",
+      name: "The Four Agreements",
+      price: "$7.74",
+      date: 17,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81XQ1+piiiL._AC_UL127_SR127,127_.jpg",
+      name: "All Good People Here: A Novel",
+      price: "$18.51",
+      date: 18,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL127_SR127,127_.jpg",
+      name: "I Love You to the Moon and Back",
+      price: "$4.31",
+      date: 19,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/91Iuhbh7aIL._AC_UL127_SR127,127_.jpg",
+      name: "The Final Gambit ",
+      price: "$14.11",
+      date: 20,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/81EVdWdmOKL._AC_UL127_SR127,127_.jpg",
+      name: "Brown Bear, Brown Bear, What Do You See?",
+      price: "$4.55",
+      date: 21,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81qROMFiwmL._AC_UL127_SR127,127_.jpg",
+      name: "Little Blue Truck's Halloween",
+      price: "$6.32",
+      date: 22,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/61kPERVIR4L._AC_UL127_SR127,127_.jpg",
+      name: "Confess: A Novel",
+      price: "$11.43",
+      date: 23,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61Bdp7XZhDL._AC_UL127_SR127,127_.jpg",
+      name: "The Silent Patient",
+      price: "$11.91",
+      date: 24,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/91Yy9b1PseL._AC_UL127_SR127,127_.jpg",
+      name: "Danielle Walker's Healthy in a Hurry",
+      price: "$24.99",
+      date: 25,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61clZgj1xZL._AC_UL127_SR127,127_.jpg",
+      name: "The Great Reset",
+      price: "$22.75",
+      date: 26,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81I2+bgGMTL._AC_UL127_SR127,127_.jpg",
+      name: "Battle for the American Mind",
+      price: "$17.99",
+      date: 27,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81xT2mdyL7L._AC_UL127_SR127,127_.jpg",
+      name: "I Love You Like No Otter",
+      price: "5.37",
+      date: 28,
+      deal: false,
+
+      // 5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "	https://images-na.ssl-images-amazon.com/images/I/71lwvQIGI8L._AC_UL127_SR127,127_.jpg",
+      name: "The Return",
+      price: "$17.38",
+      date: 29,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
+    },
+    {
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/81823bTjKHL._AC_UL127_SR127,127_.jpg",
+      name: "The Last Thing He Told Me: A Novel",
+      price: "$13.9",
+      date: 30,
+      deal: false,
+
+      // 4.5
+      rating: `<div class="ratings">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-half text-warning"></i>
+            </div>`,
     },
   ];
 }
@@ -35,13 +524,19 @@ console.log(
 
 // Looping
 books().forEach((book) => {
-  let p = document.createElement("p");
-  p.innerHTML = book.name;
-  document.body.appendChild(p);
   let img = document.createElement("img");
   img.src = book.cover;
   document.body.appendChild(img);
+
+  let p = document.createElement("p");
+  p.innerHTML = book.name;
+  document.body.appendChild(p);
+
   let div = document.createElement("div");
   div.innerHTML = book.rating;
   document.body.appendChild(div);
+
+  let span = document.createElement("span");
+  span.innerHTML = book.price;
+  document.body.appendChild(span);
 });
