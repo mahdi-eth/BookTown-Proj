93120f77a68d65b022c10fbb96073bde922c8de6
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-09-11 16:37:23 +0430

    Delete BookTown---Proj directory

--- BookTown---Proj/ProductsPage/LinkedHTMLs/best.js
+++
@@ -1,9 +0,0 @@
-import books from "../../DataBase/Data.js";
-import productMaker from "../defaultScript.js";
-
-const rating = books().sort(function (a, b) {
-  let rateA = a.rating,
-    rateB = b.rating;
-  return rateB - rateA;
-});
-productMaker(rating);
